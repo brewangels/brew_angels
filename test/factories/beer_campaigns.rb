214488669c7785cd43640583745d5ec192587ffe
Iuri Fernandes <iurifq@gmail.com>
2017-09-30 18:46:43 +0200

    Seed one beer_campaign

--- test/factories/beer_campaigns.rb
+++ test/factories/beer_campaigns.rb
@@ -1,5 +1,14 @@
 FactoryGirl.define do
   factory :beer_campaign do
-    
+    brewery
+
+    name "German IPA"
+    beer_type "IPA"
+    city "Berlin"
+    short_description "short description"
+    long_description "long description long description long description long description long description long description long description long description long description long description long description"
+    deadline Time.now
+    goal_cents 1000000
+    goal_currency 'EUR'
   end
 end
